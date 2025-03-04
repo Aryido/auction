a2b3cd7b31ed4c0f336b0367387a62f4baf7ceed
李昀陽 <yunyanglee@henrylee.local>
2025-03-05 00:33:56 +0800

    Feat: output privatekey file path

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,3 +2,8 @@ output "addresses" {
   description = "List of address values (e.g. [\"1.2.3.4\"])"
   value       = module.global_addresses.addresses
 }
+
+output "private_key_file" {
+  description = "A PrivateKey file's path for ansible SSH"
+  value       = "~/.ssh/${local.private_key_filename}"
+}
